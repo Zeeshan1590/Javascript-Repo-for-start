d99fe0ad46fe3711a5e5f8dbebb1988b39d7af3d
Zeeshan1590 <muhammadzeeshansabir4@gmail.com>
2024-11-22 17:54:12 +0000

    change

--- Start.js
+++ Start.js
@@ -1,1 +1,72 @@
 console.log("This is my first code in java script")
+// // // // // let name = "Zeeshan Sabir"
+// // // // // console.log(name);
+// // // // // age = 25;
+// // // // // age = 30
+// // // // // // console.log(age);
+
+
+// // // // // {
+// // // // //     var that ="let in block"
+// // // // // }
+// // // // // const pi= 3.146;
+// // // // // console.table([name,age,that,pi])
+// // // // // const name= "Zeeshan";
+// // // // // const age= 20;
+// // // // // console.log(`My name is ${name} and age is ${age}`);
+
+
+// // // // name= "Sabir"
+// // // // let age= 30;
+// // // // var id=2
+// // // // console.table([name,age,id])
+
+// // // // {
+// // // //     const name= "Zeeshan"
+// // // //     let age= 20;
+// // // //     var id= 1
+// // // //     console.table([name,age,id])
+    
+// // // // }
+
+// // //  {
+// // //     var varVariable = "I am function scoped";
+// // //     let letVariable = "I am block scoped";
+// // //     const constVariable = "I am block scoped too";
+// // //     console.log(constVariable); // Error: letVariable is not defined
+// // //     if (true) {
+// // //         var varVariable = "I can be accessed anywhere in this function";
+// // //         let letVariable = "I am only accessible in this block";
+        
+// // //         console.log(letVariable); // Block-scoped letVariable
+        
+// // //     console.log(varVariable); // Accessible
+// // //     console.log(letVariable); // Error: letVariable is not defined
+    
+// // //      /* let and const variables are only accesible in block code, while var variable
+// // //      can be accesd any where out of block code but not out of functions */
+// // // }
+// // //  }
+
+
+// // const a="tring";
+// // console.log(typeof a);
+
+// //name= 'Xmanise'// Error : Cannot access 'name' before initialization
+// let name= "Xmanise"
+// console.log(name);
+
+// age= 20;
+// console.log(age);
+// var age= 24;
+
+// // id= 2; //Cannot access 'id' before initialization
+// console.log(age);
+// const id= 4;
+
+// const person ={name: "Zeeshan", age: 20}
+// person.age= 23;
+// // person= 34//Assignment to constant variable.
+// console.log(person.age,person.name,person);
+  
+
